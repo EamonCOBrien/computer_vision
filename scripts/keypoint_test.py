e4678d7af72e6af8c99fa3c309831f0df647ae1d
epan547 <epan547@gmail.com>
2020-11-01 13:26:02 -0500

    Adding more images

--- scripts/keypoint_test.py
+++ scripts/keypoint_test.py
@@ -12,7 +12,7 @@ import math
 
 class ImageClassifier():
 	def __init__(self):
-		self.query = self.load_image('../imgs/skittles_light.jpg') 
+		self.query = self.load_image('../imgs/snickers_query.jpg') 
 		candy_names = ['skittles', 'snickers', 'nerds', 'reeses', 'almondjoy']
 		train_imgs = self.get_data(candy_names)
 		avgs = np.zeros(len(train_imgs))
@@ -57,10 +57,12 @@ class ImageClassifier():
 		return avg
 
 	def run(self):
+		# Iterate through all the images
 		for index, img in enumerate(self.train['imgs']):
 			avg = self.ORB_detection(self.query, img)
 			self.train.at[index, 'avgs'] = avg
 		print("List of averages", self.train['avgs'])
+
 		# Get the minimum avg (best match)
 		min_index = self.train['avgs'].argmin()
 		print("Best match: ", self.train['names'][min_index])
